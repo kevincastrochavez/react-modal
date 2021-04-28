29ef382b2f2cd31870f1d0e1bc8e08625c24e23f
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-27 18:00:55 -0600

    finished react modal with styled components

---
+++ src/components/Modal.js
@@ -0,0 +1,150 @@
+import React, { useCallback, useEffect, useRef } from "react";
+import { useSpring } from "@react-spring/core";
+import styled from "styled-components";
+import { MdClose } from "react-icons/md";
+
+import Camera from "./modal.jpg";
+import { animated } from "@react-spring/web";
+
+function Modal({ showModal, setShowModal }) {
+  const modalRef = useRef();
+
+  const animation = useSpring({
+    config: {
+      duration: 250,
+    },
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(0%)` : `translateY(-100%)`,
+  });
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === "Escape" && showModal) {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+
+    return () => document.removeEventListener("keydown", keyPress);
+  }, [keyPress]);
+
+  return (
+    <>
+      {showModal ? (
+        <Background ref={modalRef} onClick={closeModal}>
+          <animated.div style={animation}>
+            <ModalWrapper showModal={showModal}>
+              <ModalImg src={Camera} alt="Camera" />
+
+              <ModalContent>
+                <h1>Are you ready?</h1>
+                <p>Get exclusive access to our next lunch</p>
+                <button>Join Now</button>
+              </ModalContent>
+              <CloseModalButton
+                aria-label="Close modal"
+                onClick={() => setShowModal((prev) => !prev)}
+              />
+            </ModalWrapper>
+          </animated.div>
+        </Background>
+      ) : (
+        ""
+      )}
+    </>
+  );
+}
+
+export default Modal;
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalWrapper = styled.div`
+  max-width: 800px;
+  height: 500px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+
+  @media (max-width: 768px) {
+    display: flex;
+    flex-direction: column-reverse;
+    height: 90vh;
+  }
+`;
+
+const ModalImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px 0 0 10px;
+  background: #000;
+  object-fit: cover;
+
+  @media (max-width: 768px) {
+    height: 50%;
+    border-radius: 0 0 10px 10px;
+  }
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+
+  p {
+    margin-bottom: 1rem;
+  }
+
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+
+  @media (max-width: 768px) {
+    height: 50%;
+
+    button {
+      margin-top: 20px;
+    }
+  }
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
